a037bc4602e05d39251983d2b21e0a060a830b62
mithai01234 <146544284+mithai01234@users.noreply.github.com>
2023-12-12 19:54:46 +0530

    Update views.py

--- userlist/views.py
+++ userlist/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 # Create your views here.
+from rest_framework.response import Response
 from django.shortcuts import render, redirect, get_object_or_404
 from registration.models import CustomUser
 from rest_framework import generics
@@ -48,3 +49,20 @@ def suspend_user(request, catagory_id):
 
 
     return redirect('userlist')  # Redirect to your category list view
+class UserDeleteView(generics.DestroyAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = CustomUserSerializer
+      # You can define your own permission class here if needed
+
+    def destroy(self, request, *args, **kwargs):
+        user_id = request.query_params.get('user_id')
+
+        if user_id is not None:
+            try:
+                user = CustomUser.objects.get(id=user_id)
+                user.delete()
+                return Response({"message": "User deleted successfully"}, status=204)
+            except CustomUser.DoesNotExist:
+                return Response({"error": "User not found"}, status=404)
+        else:
+            return Response({"error": "User ID is required as a query parameter"}, status=400)
